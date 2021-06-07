004040c8901b798dbe9d4049032b2eed816cc774
Ygal Blum <ygal.blum@gmail.com>
2021-06-07 19:07:35 -0400

    Sample application

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ctera/ctera-gateway-csi
+
+go 1.15
+
+require (
+	github.com/antihax/optional v1.0.0
+	github.com/ctera/ctera-gateway-csi/pkg/ctera v1.0.0
+)
+
+replace github.com/ctera/ctera-gateway-csi/pkg/ctera v1.0.0 => ./pkg/ctera
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	// "strings"
+	// "time"
+
+	// "github.com/antihax/optional"
+	ctera "github.com/ctera/ctera-gateway-csi/pkg/ctera"
+)
+
+func main() {
+	// grab settings from environment
+	address := os.Getenv("CTERA_GATEWAY_ADDRESS")
+	username := os.Getenv("CTERA_GATEWAY_USER")
+	password := os.Getenv("CTERA_GATEWAY_PASSWORD")
+
+	// create golang client specific configuration
+	configuration := ctera.NewConfiguration()
+	configuration.Host = fmt.Sprintf("%s:9090", address)
+	configuration.Servers = ctera.ServerConfigurations{
+		{
+			URL: fmt.Sprintf("http://%s:9090/v1.0", address),
+			Description: "Main address",
+		},
+	}
+
+	client := ctera.NewAPIClient(configuration)
+
+	unauth := context.Background()
+	credentials := *ctera.NewCredentials(username, password)
+
+	jwt, _, err := client.LoginApi.LoginPost(unauth).Credentials(credentials).Execute()
+	if err != nil {
+		fmt.Println(err)
+
+		os.Exit(1)
+	}
+
+	// Instana uses the `apiToken` prefix in the `Authorization` header
+	auth := context.WithValue(unauth, ctera.ContextAccessToken, jwt)
+
+	searchFieldResults, _, err := client.UsersApi.UsersGetExecute(client.UsersApi.UsersGet(auth))
+	if err != nil {
+		fmt.Println(err)
+
+		os.Exit(1)
+	}
+
+	fmt.Println("Available search fields supported by Instana:")
+	for _, field := range searchFieldResults {
+		fmt.Println(fmt.Sprintf("%s", field.Username))
+	}
+}
