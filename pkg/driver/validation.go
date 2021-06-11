c1a3cccc464ff01e1c3c4275327dee3031fc9f25
Ygal Blum <ygal.blum@gmail.com>
2021-06-11 10:42:36 -0400

    Implement validator

--- pkg/driver/validation.go
+++ pkg/driver/validation.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2019 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package driver
 
 import (
@@ -22,7 +6,7 @@ import (
 
 func ValidateDriverOptions(options *DriverOptions) error {
 	if err := validateMode(options.mode); err != nil {
-		return fmt.Errorf("Invalid mode: %v", err)
+		return fmt.Errorf("invalid mode: %v", err)
 	}
 
 	return nil
@@ -30,7 +14,7 @@ func ValidateDriverOptions(options *DriverOptions) error {
 
 func validateMode(mode Mode) error {
 	if mode != AllMode && mode != ControllerMode && mode != NodeMode {
-		return fmt.Errorf("Mode is not supported (actual: %s, supported: %v)", mode, []Mode{AllMode, ControllerMode, NodeMode})
+		return fmt.Errorf("mode is not supported (actual: %s, supported: %v)", mode, []Mode{AllMode, ControllerMode, NodeMode})
 	}
 
 	return nil
